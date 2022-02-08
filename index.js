98aea2c679767868e40de289efcc665433d54072
CFSilveira <carloss.87@gmail.com>
2022-02-08 13:06:13 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);      
+  }  
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length || pos < 0) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.at(-1);
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
 
-  avg() {}
-}
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } 
+    return this.items.reduce((a, b) => a + b); 
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+    return this.items.reduce((a, b) => a + b) / this.length; 
+  }
+  }
+
 
 module.exports = SortedList;
